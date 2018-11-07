e2efa34db81b2bb0c232b1f880349fed104deeff
zych1751 <zych1751@naver.com>
2018-11-08 00:36:05 +0900

    게시판 목록 불러오기

--- src/containers/Board/Board.tsx
+++ src/containers/Board/Board.tsx
@@ -1,16 +1,84 @@
 import * as React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+import { PostList } from './PostList/PostList';
+import './Board.scss';
+import axios from 'axios';
 
 export namespace Board {
   export interface Props {
 
   }
+
+  export interface State {
+    boards: Array<BoardInfo>
+  }
+
+  export interface BoardInfo {
+    id: number,
+    name: string,
+    urlPath: string
+  }
 }
 
-export class Board extends React.Component<Board.Props> {
-  public render() {
+export class Board extends React.Component<Board.Props, Board.State> {
+  constructor(props: Board.Props) {
+    super(props);
+
+    this.state = {
+      boards: []
+    };
+  }
+  componentDidMount() {
+    axios({
+      url: apiUrl,
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/graphql'
+      },
+      data: `query {
+        boards {
+          id,
+          name,
+          urlPath
+        }
+      }`
+    }).then((msg) => {
+      // TODO: data가 BoardInfo 의 Array인지 typing
+      const data = msg.data.data.boards;
+      this.setState({
+        boards: data
+      })
+    }).catch((msg) => {
+    });
+  }
+
+  render() {
     return (
       <div>
-        여기는 게시판페이지
+        <div className="board-nav">
+          <ul>
+            {this.state.boards.map((board) => {
+              return (
+                <li key={board.id}><Link to={`/board/${board.urlPath}`}>{board.name}</Link></li>
+              )
+            })}
+          </ul>
+        </div>
+        <div className="board-content">
+          <Switch>
+            <Route exact path="/board"
+                   render={()=>(<PostList type="notice" typeInteger={1}/>)}
+            />
+            {this.state.boards.map((board) => {
+              return (
+                <Route exact path={`/board/${board.urlPath}`}
+                       render={()=>(<PostList type={board.urlPath} typeInteger={board.id}/>)}
+                       key={board.id}
+                />
+              )
+            })}
+          </Switch>
+        </div>
       </div>
     );
   }
