b34e93f527d046c4cf31e5c7235ecefca93ec813
Jjungs <jeongy95@naver.com>
2018-11-11 17:15:40 +0900

    nav bar 분리

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -6,6 +6,7 @@ import {compose, Dispatch} from 'redux';
 import {AuthenticationActions} from '../../actions';
 import { returntypeof } from 'react-redux-typescript';
 import { connect } from 'react-redux';
+import { NavBar } from '../../components/Header/NavBar'
 
 const mapStateToProps = (state: RootState) => ({
   isLogin: state.authentication.status.isLogin
@@ -29,77 +30,13 @@ class Header extends React.Component<Props, State> {
     this.handleLogout = this.handleLogout.bind(this);
   }
 
-  handleLogout() {
+  handleLogout() : void {
     this.props.logoutRequest();
   }
 
-  render(): JSX.Element {
-    let authenticationButton = (
-      <li className="nav-item">
-        <Link to="/login" className="nav-link">
-          <button className="btn btn-outline-secondary">
-            로그인
-          </button>
-        </Link>
-      </li>
-    );
-
-    let infoButton = (
-      <li className="nav-item">
-        <Link to="/register" className="nav-link nav-underline">
-          가입 신청
-        </Link>
-      </li>
-    );
-
-    if(this.props.isLogin) {
-      authenticationButton = (
-        <li className="nav-item">
-          <a className="nav-link">
-            <button className="btn btn-outline-secondary" onClick={() => this.handleLogout()}>
-              로그아웃
-            </button>
-          </a>
-        </li>
-      );
-
-      infoButton = (
-        <li className="nav-item">
-          <Link to="/info" className="nav-link nav-underline">
-            내 정보
-          </Link>
-        </li>
-      );
-    }
-
+  render() {
     return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-light header">
-          <Link className="navbar-brand logo" to="/">PoolC</Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-                  aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div id="navbarNavDropdown" className="collapse navbar-collapse">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link to="/board" className="nav-link nav-underline">
-                  게시판
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/project" className="nav-link nav-underline">
-                  프로젝트
-                </Link>
-              </li>
-            </ul>
-            <ul className="navbar-nav">
-              {infoButton}
-              {authenticationButton}
-            </ul>
-          </div>
-        </nav>
-      </div>
+        <NavBar isLogin={this.props.isLogin} onLogout={this.handleLogout}/>
     );
   }
 }
