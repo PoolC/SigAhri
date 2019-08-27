dae8b3aecf54bcfbd2fb7b800bb915924a23050a
Jjungs7 <jeongy95@naver.com>
2019-08-27 17:30:02 +0900

    updated president info && fixed build error for omit

--- types/global.d.ts
+++ types/global.d.ts
@@ -7,7 +7,7 @@ declare module '*.css' {
 }
 
 // Omit type https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-377567046
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+//type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 type PartialPick<T, K extends keyof T> = Partial<T> & Pick<T, K>;
 
 // webpack DefinePlugin
